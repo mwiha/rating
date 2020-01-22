0e9e7f32e1f2dc2bab7fef7a3902e47b7033b8f3
Alice Mwihaki <alicemwihaki99@gmail.com>
2020-01-22 11:52:46 +0300

    import sqlalchemy and  create its instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 app.config["SECRET_KEY"] = "efec7e52ccb4a3e86ec302b43fce90fa1c167ba1"
 
 
