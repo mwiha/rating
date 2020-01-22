9a635cc53617809df3d0f305ce3119fd70d58763
Alice Mwihaki <alicemwihaki99@gmail.com>
2020-01-22 12:03:41 +0300

    add a user model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,23 @@
-from app import db
+from datetime import datetime
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from app import db, app
+from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db,Model,UserMixin):
+    id = db.Column(db.Interger , primary_key=True)
+    username =db.Column(db.string(20),unique=True,nullable=False)
+    email = db.Column(db.String(120), unique=True , nullable=False)
+    password =db.Column(db.String(60),nullable=False)
+    
+    
+
+
+
 
 
 
