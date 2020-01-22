943df830f121c46bbeaf3f86a0bb5e4f410f0bea
Alice Mwihaki <alicemwihaki99@gmail.com>
2020-01-22 13:03:36 +0300

    validate data on signup and login

--- app/models.py
+++ app/models.py
@@ -15,6 +15,6 @@ class User(db.Model,UserMixin):
     password = db.Column(db.String(60), nullable=False)
     
 
-    def __repr__(self):
-        return f"User('{self.username}','{self.email}','{self.image_file}')"
+    # def __repr__(self):
+    #     return f"User('{self.username}','{self.email}')"
     
--- app/users/forms.py
+++ app/users/forms.py
@@ -13,15 +13,15 @@ class RegistrationForm(FlaskForm):
         "Password", validators=[DataRequired(), EqualTo("password")])
     submit = SubmitField("Sign Up")
     
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user:
-            raise ValidationError("That username is taken please choose another")
+    # def validate_username(self, username):
+    #     user = User.query.filter_by(username=username.data).first()
+    #     if user:
+    #         raise ValidationError("That username is taken please choose another")
         
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user:
-            raise ValidationError("That email is taken please choose another")
+    # def validate_email(self, email):
+    #     user = User.query.filter_by(email=email.data).first()
+    #     if user:
+    #         raise ValidationError("That email is taken please choose another")
         
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
