209e0cd85d473898dc01ad31a0e87e8efa33a911
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-01-22 17:28:21 +0300

    add flash messages on succesfull login and signup

--- app/users/views.py
+++ app/users/views.py
@@ -21,7 +21,7 @@ def register():
                     email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
-        flash('Your account has been created! You are now able to log in', 'success')
+        flash('Your account has been created! You are now able to log in.', 'success')
         return redirect(url_for('users.login'))
     return render_template('register.html', title='Register', form=form)
 
